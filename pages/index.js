2c631bf38abaaeda94f9b6b4fd4885a7d3102634
Rosicre <94906196+Rosicre@users.noreply.github.com>
2023-12-08 03:41:10 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
   return (
     <div>
       <h1>Querida Família</h1>
       <p>
-  
-        Hoje, enquanto construímos este projeto juntos, quero expressar o amor e a gratidão que sinto por cada um de vocês. Este projeto não é apenas sobre construir um site; é sobre construir um legado de amor, fé e valores que transcenderá o tempo.
-
-        Cada novo dia ao lado de vocês é uma inspiração. O amor e apoio que compartilhamos são as forças motrizes que nos impulsionam a superar desafios e celebrar conquistas. Juntos, construímos uma vida repleta de significado e propósito.
-
-        Quero lembrar a cada um de nós que somos a razão para acordarmos todas as manhãs com corações cheios de determinação. Ver todos vocês crescerem e se desenvolverem é uma bênção que dá sentido à minha vida.
-
-        À medida que avançamos nesta jornada, lembrem-se de que o compromisso que tenho com cada um de vocês é inabalável. Estamos unidos não apenas por laços familiares, mas por um amor que é eterno e imensurável.
-
-        Este projeto transcende códigos e designs; é uma manifestação da nossa determinação coletiva. Ao refletirem sobre este projeto em algum momento, espero que sintam-se inspirados a continuar construindo um futuro permeado pela fé, 
-        
-        cultivando um amor profundo a Deus que nos guia em cada passo. Que esta jornada seja iluminada pela luz da nossa fé, fortalecendo-nos e inspirando-nos a superar desafios com confiança e a construir um legado de amor e dedicação àquilo que acreditamos.
-
-
-        Com carinho 😎 e gratidão, amo vocês 😘!!!
+        Hoje, enquanto construímos este projeto juntos, quero expressar o amor e
+        a gratidão que sinto por cada um de vocês. Este projeto não é apenas
+        sobre construir um site; é sobre construir um legado de amor, fé e
+        valores que transcenderá o tempo. Cada novo dia ao lado de vocês é uma
+        inspiração. O amor e apoio que compartilhamos são as forças motrizes que
+        nos impulsionam a superar desafios e celebrar conquistas. Juntos,
+        construímos uma vida repleta de significado e propósito. Quero lembrar a
+        cada um de nós que somos a razão para acordarmos todas as manhãs com
+        corações cheios de determinação. Ver todos vocês crescerem e se
+        desenvolverem é uma bênção que dá sentido à minha vida. À medida que
+        avançamos nesta jornada, lembrem-se de que o compromisso que tenho com
+        cada um de vocês é inabalável. Estamos unidos não apenas por laços
+        familiares, mas por um amor que é eterno e imensurável. Este projeto
+        transcende códigos e designs; é uma manifestação da nossa determinação
+        coletiva. Ao refletirem sobre este projeto em algum momento, espero que
+        sintam-se inspirados a continuar construindo um futuro permeado pela fé,
+        cultivando um amor profundo a Deus que nos guia em cada passo. Que esta
+        jornada seja iluminada pela luz da nossa fé, fortalecendo-nos e
+        inspirando-nos a superar desafios com confiança e a construir um legado
+        de amor e dedicação àquilo que acreditamos. Com carinho 😎 e gratidão,
+        amo vocês 😘!!!
       </p>
     </div>
   );
 }
 
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
