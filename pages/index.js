29f09a16824575f15120da37482b68a20a40ce61
Rosicre <94906196+Rosicre@users.noreply.github.com>
2023-12-21 18:53:43 +0000

    Atualizando

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,17 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTools } from "@fortawesome/free-solid-svg-icons";
+import "./UnderConstructionPage.css"; // Importe o arquivo CSS
 
 function UnderConstructionPage() {
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <FontAwesomeIcon icon={faTools} size="4x" style={{ color: "#3498db" }} />
-      <h1 style={{ marginTop: "20px" }}>Página em Construção</h1>
+    <div className="under-construction-container">
+      <div className="construction-icon">&#x1F6A7;</div>
+      <h1>Página em Construção</h1>
       <p>
         Estamos trabalhando arduamente para trazer uma experiência incrível em
         breve. Enquanto isso, agradeço sua paciência e apoio.
       </p>
-      <div
-        style={{
-          width: "100px",
-          height: "2px",
-          backgroundColor: "#3498db",
-          margin: "20px auto",
-        }}
-      ></div>
-      <p style={{ fontStyle: "italic", color: "#777" }}>
+      <div className="construction-line"></div>
+      <p className="quote">
         "A construção do futuro está em andamento."
       </p>
     </div>
