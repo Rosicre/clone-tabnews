824e9c6ce027ff98831df7b1c8294d5bb6e711d4
Rosicre <94906196+Rosicre@users.noreply.github.com>
2023-12-21 14:13:37 +0000

    adiciona o arquivo

--- models/calculadora.js
+++ models/calculadora.js
@@ -1,8 +1,8 @@
-function somar (numero1, numero2){
+function somatorio (numero1, numero2){
   if (typeof numero1 !== "number"){
     return "Error";
   }
   return numero1 + numero2;
 }
 
-exports.somar = somar;
+exports.somar = somatorio;
