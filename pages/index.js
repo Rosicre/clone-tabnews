9d22c7ff82e25673180d98bc35a919fdb0cc414e
Rosicre <94906196+Rosicre@users.noreply.github.com>
2023-12-21 14:29:28 +0000

    Atualizando a Página index.s

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ function UnderConstructionPage() {
         breve. Enquanto isso, agradeço sua paciência e apoio.
       </p>
       <img
-        src="https://images.pexels.com/photos/18244271/pexels-photo-18244271/free-photo-of-rural-tempo-arvore-jarda.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="Under Construction"
+        src="https://images.pexels.com/photos/3489129/pexels-photo-3489129.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="Catharina Boutique"
         style={{ maxWidth: "100%", height: "auto", marginTop: "20px" }}
       />
     </div>
