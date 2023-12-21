f068002aa8af75da48b312a581e73134485522c1
Rosicre <94906196+Rosicre@users.noreply.github.com>
2023-12-21 14:16:55 +0000

    adiciona o arquivo

--- testes/calculadora.test.js
+++ testes/calculadora.test.js
@@ -1,8 +1,8 @@
 const calculadora = require("../models/calculadora.js")
 
 test ("somar 2 + 2 deveria retornar 4", () => {
-  const resultado = calculadora.somar(2, 2);
-  expect(resultado).toBe(4);
+  const resultado = calculadora.somar(4, 4);
+  expect(resultado).toBe(8);
 });
 
 test ("somar 5 + 100 deveria retornar 105", () => {
