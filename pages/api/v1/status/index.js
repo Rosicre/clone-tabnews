92274568de426fb4439805f508c28f6ad25b26df
Rosicre <94906196+Rosicre@users.noreply.github.com>
2024-01-07 13:12:41 +0000

    atualizando

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
 function status(request, response) {
   response
     .status(200)
-    .json({ chave: "Encostando a mão no Protocolo HTTP sem maquiagem" });
+    .json({ chave: "Encostando a mão no Protocolo HTTP sem maquiagem  ...." });
 }
 
 export default status;
