b18e7041069631f0833da9808270a1f4d354f82c
Rosicre <94906196+Rosicre@users.noreply.github.com>
2025-09-19 17:39:08 +0000

    refactor: padroniza os Controllers dos endpoints `/migrations` e `status`
    Adiciona 2 novos erros customizados: `MethodNotAllowedError` e `ServiceError`.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,53 +1,57 @@
+import { createRouter } from "next-connect";
 import migrationsRunner from "node-pg-migrate";
 import { resolve } from "node:path";
-import database from "infra/database";
+import database from "infra/database.js";
+import controller from "infra/controller.js";
+const router = createRouter();
 
-export default async function migrations(request, response) {
-  const allowedMethods = ["GET", "POST"];
-  if (!allowedMethods.includes(request.method)) {
-    return response.status(405).json({
-      error: `Method "${request.method}" not allowed`,
-    });
-  }
+router.get(getHandler);
+router.post(postHandler);
 
+export default router.handler(controller.errorHandlers);
+
+const defaultMigrationsOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function getHandler(request, response) {
   let dbClient;
 
   try {
     dbClient = await database.getNewClient();
 
-    const defaultMigrationsOptions = {
-      dbClient: dbClient,
-      dryRun: true,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
+    const pendingMigrations = await migrationsRunner({
+      ...defaultMigrationsOptions,
+      dbClient,
+    });
 
-    if (request.method === "GET") {
-      //console.log("Entrou no GET");
-      const pendingMigrations = await migrationsRunner(
-        defaultMigrationsOptions,
-      );
-      return response.status(200).json(pendingMigrations);
+    return response.status(200).json(pendingMigrations);
+  } finally {
+    await dbClient.end();
+  }
+}
+
+async function postHandler(request, response) {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrateMigrations = await migrationsRunner({
+      ...defaultMigrationsOptions,
+      dbClient,
+      dryRun: false,
+    });
+
+    if (migrateMigrations.length > 0) {
+      return response.status(201).json(migrateMigrations);
     }
 
-    if (request.method === "POST") {
-      //console.log("Entrou no POST");
-      const migrateMigrations = await migrationsRunner({
-        ...defaultMigrationsOptions,
-        dryRun: false,
-      });
-
-      if (migrateMigrations.length > 0) {
-        return response.status(201).json(migrateMigrations);
-      }
-
-      return response.status(200).json(migrateMigrations);
-    }
-  } catch (error) {
-    console.error(error);
-    throw error;
+    return response.status(200).json(migrateMigrations);
   } finally {
     await dbClient.end();
   }
