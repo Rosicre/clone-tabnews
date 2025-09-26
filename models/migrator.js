b1929eb302c0f6f7d5e8480b3b4871778ebc9425
Rosicre <94906196+Rosicre@users.noreply.github.com>
2025-09-26 14:50:58 +0000

    feat: hide `migrator` logs

--- models/migrator.js
+++ models/migrator.js
@@ -6,7 +6,7 @@ const defaultMigrationsOptions = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
