bf1cca03216da89c76b2f8600c15350f9b085c31
Rosicre <94906196+Rosicre@users.noreply.github.com>
2025-11-06 17:49:08 +0000

    feat: implement `/api/v1/user` endpoint with session renewal

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -3,9 +3,7 @@ import controller from "infra/controller.js";
 import user from "models/user.js";
 
 const router = createRouter();
-
 router.post(postHandler);
-
 export default router.handler(controller.errorHandlers);
 
 async function postHandler(request, response) {
