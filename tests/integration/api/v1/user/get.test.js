1fc6a265afe24d3c80495a859f3199bca02123ed
Rosicre <94906196+Rosicre@users.noreply.github.com>
2025-11-10 00:55:29 +0000

    feat: add `features` column to `users` table

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -36,6 +36,7 @@ describe("GET /api/v1/user", () => {
         id: createdUser.id,
         username: "UserWithValidSession",
         email: createdUser.email,
+        features: [],
         password: createdUser.password,
         created_at: createdUser.created_at.toISOString(),
         updated_at: createdUser.updated_at.toISOString(),
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -31,6 +31,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "rosicreferreira",
         email: "rosicreferreira@gmail.com",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
