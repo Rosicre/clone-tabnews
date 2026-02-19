93e768511c43058d5db1a82e0ac5270313e8a3f8
Rosicre <94906196+Rosicre@users.noreply.github.com>
2026-02-19 23:55:20 +0000

    feat: require `update:user` to access `/api/v1/users/[username]`

--- models/activation.js
+++ models/activation.js
@@ -102,6 +102,7 @@ async function activateUserByUserId(userId) {
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
+    "update:user",
   ]);
   return activatedUser;
 }
--- tests/integration/api/v1/activations/[token_id]/patch.test.js
+++ tests/integration/api/v1/activations/[token_id]/patch.test.js
@@ -141,6 +141,7 @@ describe("PATCH /api/v1/activations/[token_id]", () => {
       expect(activatedUser.features).toEqual([
         "create:session",
         "read:session",
+        "update:user",
       ]);
     });
 
