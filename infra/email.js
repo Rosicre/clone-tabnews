9a29049e7b927ecfb7b38adf479db0a383fad66c
Rosicre <94906196+Rosicre@users.noreply.github.com>
2026-03-10 03:05:58 +0000

    refactor: improve error loggin in `email.send()`

--- infra/email.js
+++ infra/email.js
@@ -1,4 +1,6 @@
+import { th } from "@faker-js/faker/.";
 import nodemailer from "nodemailer";
+import { ServiceError } from "./errors.js";
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
@@ -12,7 +14,16 @@ const transporter = nodemailer.createTransport({
 });
 
 async function send(mailOptions) {
-  await transporter.sendMail(mailOptions);
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    throw new ServiceError({
+      message: "Não foi possível enviar o email.",
+      action: "Verifique se o serviço de email está disponível.",
+      cause: error,
+      context: mailOptions,
+    });
+  }
 }
 
 const email = {
