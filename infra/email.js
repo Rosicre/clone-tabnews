916ff7460be5926e50165e898a095cecdbd80444
Rosicre <94906196+Rosicre@users.noreply.github.com>
2026-03-22 23:39:02 +0000

    fix: update dependencies to resolve CVE vulnerabilities

--- infra/email.js
+++ infra/email.js
@@ -5,8 +5,8 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
   port: process.env.EMAIL_SMTP_PORT,
   auth: {
-    user: process.env.EMAIL_SMTP_HOST_USER,
-    pass: process.env.EMAIL_SMTP_HOST_PASSWORD,
+    user: process.env.EMAIL_SMTP_USER,
+    pass: process.env.EMAIL_SMTP_PASSWORD,
   },
 
   secure: process.env.NODE_ENV === "production" ? true : false,
