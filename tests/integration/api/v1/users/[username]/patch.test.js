c0aa32172025c35cb1dd09ac65fc2660641bdae4
Rosicre <94906196+Rosicre@users.noreply.github.com>
2026-03-27 17:34:41 +0000

    test: assert `email` is updated in database after `PATCH /api/v1/users/[username]`

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -258,6 +258,10 @@ describe("PATCH /api/v1/users/[username]", () => {
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
 
       expect(responseBody.updated_at > responseBody.created_at).toBe(true);
+
+      const userInDatabase = await user.findOneByUsername(createdUser.username);
+
+      expect(userInDatabase.email).toBe("uniqueEmail2@gmail.com");
     });
 
     test("With new `password`", async () => {
